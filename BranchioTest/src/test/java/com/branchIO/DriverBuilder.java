14934dad8783690c318086660851d58d89123e19
manjujain <manjujain@gmail.com>
2018-09-18 00:18:05 -0700

    update path

--- BranchioTest/src/test/java/com/branchIO/DriverBuilder.java
+++ BranchioTest/src/test/java/com/branchIO/DriverBuilder.java
@@ -17,8 +17,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class DriverBuilder {
 	 WebDriver driver;
      FileInputStream fis;
-   //GitFolder
-     String chromeDrPath=System.getProperty("user.home")+File.separator+"testGit"+File.separator+"BranchTest"+File.separator+"BranchioTest"+File.separator+"src"+File.separator+"test"+File.separator+"java"+File.separator+"resource"+File.separator+"chromedriver";
+   
+     String chromeDrPath=System.getProperty("user.home")+File.separator+"GitFolder"+File.separator+"BranchTest"+File.separator+"BranchioTest"+File.separator+"src"+File.separator+"test"+File.separator+"java"+File.separator+"resource"+File.separator+"chromedriver";
 	 String Firefoxdrpath = System.getProperty("user.home")+File.separator+"GitFolder"+File.separator+"BranchTest"+File.separator+File.separator+"BranchioTest"+File.separator+"src"+File.separator+"test"+File.separator+"java"+File.separator+"resource"+File.separator+"gecko.driver";
 	  
 	  String IEdrpath =  System.getProperty("user.home")+"/GitFolder/BranchTest/BranchioTest/src/test/java/resource/iedriver";
